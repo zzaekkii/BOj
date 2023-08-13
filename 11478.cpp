601c724c396909cc44950e60df007ec2ff524723
zakie <realzzaekkii@gmail.com>
2023-08-13 17:12:46 +0900

    11478번 서로 다른 부분 문자열의 개수 - 실버3 / set, substr

---
+++ 11478.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+set<string> M;
+string S;
+
+int main(){	
+	ios_base::sync_with_stdio(0);cin.tie(0);
+	cin >> S;
+	
+	int i,j,l=S.length();
+	
+	for(i=0;i<l;i++){
+		for(j=i;j<l;j++)
+			M.insert(S.substr(i,j));
+		for(j=0;j<=i;j++)
+			M.insert(S.substr(i,j));
+	}
+	
+	cout << M.size();
+}
