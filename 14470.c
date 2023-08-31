75637cfece31fb58a56edc6b4c5c9079d7f3bd5e
zakie <realzzaekkii@gmail.com>
2023-08-31 16:54:45 +0900

    14470번 전자레인지 - 브론즈4 / 수학

--- 14470.c
+++ 14470.c
@@ -5,5 +5,6 @@ int A,B,C,D,E;
 int main(){
     scanf("%d %d %d %d %d",&A,&B,&C,&D,&E);
     if(A<0)printf("%d",abs(A)*C+D+B*E);
+    else if(!A)printf("%d",D+(B-A)*E);
     else printf("%d",(B-A)*E);
 }
