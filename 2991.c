291fdf63587dd3960c4045d7e3720ad6f67466ad
zzaekkii <93702146+zzaekkii@users.noreply.github.com>
2023-09-01 23:59:43 +0900

    Create 2991.c

---
+++ 2991.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int A,B,C,D;
+int arr[3];
+int ans[3];
+int main(){
+    scanf("%d%d%d%d",&A,&B,&C,&D);
+    for(int i=0;i<3;i++){
+        scanf("%d",&arr[i]);
+        int dog1 = arr[i] % (A + B) ;
+        int dog2 = arr[i] % (C + D);
+        if (dog1 <= A && dog1 != 0) ans[i]++;
+        if (dog2 <= C && dog2 != 0) ans[i]++;
+    }
+    for(int i=0;i<3;i++)printf("%d\n",ans[i]);
+}
