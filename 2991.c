0a9e3792019b7f4675608512ea4f918ea4dee9af
zakie <realzzaekkii@gmail.com>
2023-09-02 15:25:29 +0900

    2991번 사나운 개 - 브론즈3 / 모듈러

--- 2991.c
+++ 2991.c
@@ -6,10 +6,10 @@ int main(){
     scanf("%d%d%d%d",&A,&B,&C,&D);
     for(int i=0;i<3;i++){
         scanf("%d",&arr[i]);
-        int dog1 = arr[i] % (A + B) ;
-        int dog2 = arr[i] % (C + D);
-        if (dog1 <= A && dog1 != 0) ans[i]++;
-        if (dog2 <= C && dog2 != 0) ans[i]++;
+        int dog1=arr[i]%(A+B) ;
+        int dog2=arr[i]%(C+D);
+        if(dog1<=A&&dog1)ans[i]++;
+        if(dog2<=C&&dog2)ans[i]++;
     }
     for(int i=0;i<3;i++)printf("%d\n",ans[i]);
 }
