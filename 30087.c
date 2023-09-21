abcd55588dc9152bbc9bac1c8e25302e5c574c5e
최재영 <93702146+zzaekkii@users.noreply.github.com>
2023-09-21 21:34:46 +0900

    30087번 진흥원 세미나 - 브론즈5 / 구현

---
+++ 30087.c
@@ -0,0 +1,10 @@
+char S[30];main(N){scanf("%d ",&N);while(N--){gets(S);
+switch(S[3]){
+case 'o':puts("204");break;
+case 'a':puts("207");break;
+case 'i':puts("302");break;
+case 'e':puts("B101");break;
+case 'w':puts("303");break;
+case 'r':puts("501");break;
+case 't':puts("105");break;
+}}}
