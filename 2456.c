77e992a963f70be162b08d9bcd4d36a003e11f38
최재영 <93702146+zzaekkii@users.noreply.github.com>
2023-09-23 23:59:01 +0900

    Create 2456.c

---
+++ 2456.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int N, i, f;
+int T[3], a[3];
+
+int main(){
+    scanf("%d",&N);
+    while(N--){
+        for(i=0;i<3;i++)
+            scanf("%d",&T[i]);
+            a[i]+=T[i];
+    }
+}
