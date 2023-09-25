c353902b0b16fbd6a2a2941912d2e937e88c1563
zakie <realzzaekkii@gmail.com>
2023-09-25 18:10:24 +0900

    9996번 한국이 그리울 땐 서버에 접속하지 - 실버3 / 문자열

--- 9996.c
+++ 9996.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-int N, l;
-char P[4];
+int N, lp, ls;
+char P[101];
 char S[101];
 
-int main(){
-    scanf("%d ",&N);
-    gets(P);
-    while(N--){
-        gets(S);
-        l=strlen(S);
-        puts(P[0]==S[0]&&P[2]==S[l-1]?"DA":"NE");
-    }
+int main() {
+	scanf("%d ", &N);
+	gets(P);
+	lp = strlen(P);
+	char* p = strchr(P, '*');
+	int x = p - P;
+	while (N--) {
+		gets(S);
+		_Bool f = 1;
+		ls = strlen(S);
+		if (lp - 1 > ls) {
+			puts("NE");
+			continue;
+		}
+		for (int i = 0; i <= lp - x - 1; i++)
+			if (P[lp - i] != S[ls - i]) {
+				f = 0;
+				break;
+			}
+		puts(!strncmp(P, S, x) && f ? "DA" : "NE");
+	}
 }
