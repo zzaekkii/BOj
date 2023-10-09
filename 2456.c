d22b09438cee3c0620b8df214a38d608177098da
zakie <realzzaekkii@gmail.com>
2023-10-09 21:26:36 +0900

    2456번 나는 학급회장이다 - 브론즈1 / 순위매기기

--- 2456.c
+++ 2456.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
-int N, i, f;
-int T[3], a[3];
+int N, i, f, x;
+int T, a[3], s[3];
 
 int main(){
     scanf("%d",&N);
-    while(N--){
+    while(N--)
+        for(i=0;i<3;i++){
+            scanf("%d",&T);
+            a[i]+=T;
+            s[i]+=T*T*T*T;
+        }
+    
+    int m=a[0];
+    for(i=1;i<3;i++)
+        if(m<=a[i]){
+            x=i;
+            m=a[i];
+        }
+    
+    for(i=0;i<3;i++)if(m==a[i])f++;
+    
+    if(f>1){
+        m=s[0];
+        for(i=1;i<3;i++)if(m<s[i])m=s[i];
+        int cnt=0;
         for(i=0;i<3;i++)
-            scanf("%d",&T[i]);
-            a[i]+=T[i];
+            if(m==s[i]){
+                x=i;
+                cnt++;
+            }
+        if(cnt==1)printf("%d %d",x+1,a[x]);
+        else printf("0 %d",a[x]);
     }
+    else printf("%d %d",x+1,m);
 }
