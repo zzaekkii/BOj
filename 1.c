695e0e98c24b26d2b9400e76c54b54dde068a058
zzaekkii <93702146+zzaekkii@users.noreply.github.com>
2023-11-21 23:59:52 +0900

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int N;
+
+int main(){
+    scanf("%d",&N);
+    if(N>=620)puts("Red");
+    else if(N>=590)puts("Orange");
+    else if(N>=570)puts("Yellow");
+    else if(N>=475)puts("Green");
+    else if(N>=450)puts("Blue");
+    else if(N>=425)puts("Indigo");
+    else puts("Violet");
+}
