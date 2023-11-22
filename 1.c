1cd9bdbc1e5ed8d72898c8480a89ecbb48a92096
zakie <realzzaekkii@gmail.com>
2023-11-23 00:04:40 +0900

    30676번 이 별은 무슨 색일까 - 브론즈5 / 수학

--- 1.c
+++ 1.c
@@ -6,7 +6,7 @@ int main(){
     if(N>=620)puts("Red");
     else if(N>=590)puts("Orange");
     else if(N>=570)puts("Yellow");
-    else if(N>=475)puts("Green");
+    else if(N>=495)puts("Green");
     else if(N>=450)puts("Blue");
     else if(N>=425)puts("Indigo");
     else puts("Violet");
