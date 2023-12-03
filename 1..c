3a4caeda87d83baa71943f44c43458490ceec7d8
zzaekkii <93702146+zzaekkii@users.noreply.github.com>
2023-12-03 23:59:58 +0900

    Create 1..c

---
+++ 1..c
@@ -0,0 +1,13 @@
+int main() {
+	int i, r, n;
+	char s[81];
+	scanf("%d", &r);
+	while (r--) {
+		scanf("%d %s", &n, s);
+		for (i = 0; s[i] != 0; i++) {
+			if (i == n-1)
+				continue;
+			printf("%c", s[i]);
+		}puts("");
+	}
+}
