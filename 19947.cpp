28b7545ca754b9e84529a7dbee8f9be36dc66638
zakie <realzzaekkii@gmail.com>
2023-12-21 21:42:52 +0900

    19947번 투자의 귀재 배주형 - 실버5 / DP

--- 19947.cpp
+++ 19947.cpp
@@ -9,9 +9,9 @@ int main(){
 	dp[0]=H;
     
     for(int i=1;i<=Y;i++){
-    	if(!(i%3))dp[i]=max(dp[i-1]*1.05,dp[i-3]*1.2);
-    	else if(!(i%5))dp[i]=max(dp[i-1]*1.05,dp[i-5]*1.35);
-    	else dp[i]=dp[i-1]*1.05;
+    	dp[i]=dp[i-1]*1.05;
+    	if(i>=3)dp[i]=max(dp[i],(int)(dp[i-3]*1.2));
+    	if(i>=5)dp[i]=max(dp[i],(int)(dp[i-5]*1.35));
 	}
 	cout << dp[Y];
 }
