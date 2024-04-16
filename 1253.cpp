d80acc5fb4d1472fcb7327a460f5adf2143ad551
zzaekkii <realzzaekkii@gmail.com>
2024-04-17 00:15:47 +0900

    1253번 좋다 - 골드4 / 이분탐색

---
+++ 1253.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int A[2001];
+int ans;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    
+    for(int i=0;i<N;i++)
+    	cin >> A[i];
+    	
+    sort(A,A+N);
+    
+    for(int i=0;i<N;i++){
+    	int l=0,r=N-1;
+    	while(l<r){
+    		if(A[l]+A[r]==A[i]){
+    			if(l!=i&&r!=i){
+    				ans++;
+    				break;
+				}
+				else if(l==i)l++;
+				else if(r==i)r--;
+			}
+			else if(A[l]+A[r]<A[i])l++;
+			else r--;
+		}
+	}
+	
+	cout << ans;
+}
