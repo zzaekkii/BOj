0812955e1cd4c21fe03152e49398defbf128dedc
Jae-yeong Choi <realzzaekkii@gmail.com>
2024-06-02 00:41:06 +0900

    15663번 N과 M (9) - 실버2 / 백트래킹

---
+++ 15663.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M;
+int A[9];
+bool use[9];
+int ans[9];
+
+void Dfs(int cnt){
+	if(cnt==M){
+		for(int i=0;i<M;i++)
+			cout << ans[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	
+	int prev=0;
+	for(int i=0;i<N;i++)
+		if(!use[i]&&prev!=A[i]){
+			use[i]=1;
+			ans[cnt]=prev=A[i];
+			Dfs(cnt+1);
+			use[i]=0;
+		}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> N >> M;
+	
+	for(int i=0;i<N;i++)
+		cin >> A[i];
+	
+	sort(A,A+N);	
+	
+	Dfs(0);
+}
