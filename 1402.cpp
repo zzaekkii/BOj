9b8d3fe7c42e66d8096036a3d973bd0d20ae9efc
Jae-yeong Choi <realzzaekkii@gmail.com>
2024-12-16 00:51:30 +0900

    1402번 아무래도이문제는A번난이도인것같다 - 실버5 / 애드혹

---
+++ 1402.cpp
@@ -0,0 +1,16 @@
+// 1402번 아무래도이문제는A번난이도인것같다 - 실버5 /  
+#include <bits/stdc++.h>
+using namespace std;
+
+int T, A, B; // 테스트 T번 동안 A가 A'로 변할 수 있는지 판별. 
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> T;
+    
+    while(T--){
+    	cin >> A >> B;
+    	
+    	cout << "yes\n";
+	}
+}
