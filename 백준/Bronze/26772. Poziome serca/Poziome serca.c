5b77a07a5545069d0574b52c9c14ec1697de23e5
Jae-yeong Choi <zzaekkii@gmail.com>
2025-03-09 23:29:51 +0900

    [Bronze IV] Title: Poziome serca, Time: 96 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int N;
+
+int main(){
+    scanf("%d",&N);
+    
+for(int i=0;i<N;i++)printf(" @@@   @@@  ");puts("");
+for(int i=0;i<N;i++)printf("@   @ @   @ ");puts("");
+for(int i=0;i<N;i++)printf("@    @    @ ");puts("");
+for(int i=0;i<N;i++)printf("@         @ ");puts("");
+for(int i=0;i<N;i++)printf(" @       @  ");puts("");
+for(int i=0;i<N;i++)printf("  @     @   ");puts("");
+for(int i=0;i<N;i++)printf("   @   @    ");puts("");
+for(int i=0;i<N;i++)printf("    @ @     ");puts("");
+for(int i=0;i<N;i++)printf("     @      ");puts("");        
+}
