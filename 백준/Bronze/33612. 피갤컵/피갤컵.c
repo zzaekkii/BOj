dfc952c20aa85a1c0755a5c6d02760988e70a3ad
Jae-yeong Choi <zzaekkii@gmail.com>
2025-03-21 23:59:33 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.c
@@ -0,0 +1,21 @@
+main(N){
+    scanf("%d",&N);
+    
+    switch(N){
+        case 1:
+            puts("2024 8");
+            break;
+        case 2:
+            puts("2025 3");
+            break;
+        case 3:
+            puts("2025 10");
+            break;
+        case 4:
+            puts("2026 5");
+            break;
+        case 5:
+            puts("2026 12");
+            break;
+    }
+}
