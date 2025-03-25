4031cd0b557266eef1f693177f0d80e14d777d16
Jae-yeong Choi <zzaekkii@gmail.com>
2025-03-25 20:09:17 +0900

    [Gold IV] Title: 사이클 게임, Time: 132 ms, Memory: 5540 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.c
@@ -0,0 +1,38 @@
+// 20040번 사이클 게임 - 골드5 / 유니온 파인드
+#include <stdio.h>
+
+int N, M;
+int A, B;
+int root[500001];
+int ans;
+
+int Find(int x) {
+	if (root[x] == x)return x;
+	return root[x] = Find(root[x]);
+}
+
+void Union(int x, int y) {
+	int rootX = root[x];
+	int rootY = root[y];
+
+	if (rootX != rootY)
+		root[rootY] = rootX;
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < N; i++)root[i] = i;
+
+	for (int i = 1; i <= M; i++) {
+		scanf("%d %d", &A, &B);
+
+		if (Find(A) == Find(B)) {
+			ans = i;
+			break;
+		}
+		else Union(A, B);
+	}
+
+	printf("%d", ans);
+}
