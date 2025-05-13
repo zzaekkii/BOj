e8347835444785f69dcf1f92f583b8c0c7fc4085
Jae-yeong Choi <zzaekkii@gmail.com>
2025-05-13 23:03:47 +0900

    [Bronze IV] Title: SciComLove (2025), Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/33810. SciComLove （2025）/SciComLove （2025）.c
+++ 백준/Bronze/33810. SciComLove （2025）/SciComLove （2025）.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 
-// SciComLove
+char p[]="SciComLove";
 
 char S[11];
-int up[26], low[26];
 int ans;
 
 int main(){
-    up['S'-'A']=up['C'-'A']=up['L'-'A']=1;
-    low['c'-'a']=low['i'-'a']=low['m'-'a']=low['v'-'a']=low['e'-'a']=1;
-    low['o'-'a']=2;
-    
     gets(S);
     
-    for(int i=0;S[i];i++){
-        if(S[i]>='a'){
-            if(low[S[i]-'a'])low[S[i]-'a']--;
-            else ans++;
-        }
-        else{
-            if(up[S[i]-'A'])up[S[i]-'A']--;
-            else ans++;
-        }
-    }
+    for(int i=0;S[i];i++)
+        if(S[i]!=p[i])ans++;
     
     printf("%d",ans);
 }
