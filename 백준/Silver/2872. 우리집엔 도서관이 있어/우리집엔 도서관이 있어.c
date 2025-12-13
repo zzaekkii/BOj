2d12a811cc30261e51be4747957f8bfe8796a130
Jaeyeong Choi <zzaekkii@gmail.com>
2025-12-14 00:06:46 +0900

    [Silver I] Title: 우리집엔 도서관이 있어, Time: 40 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.c
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+#define max(a,b) (((a)>(b))?(a):(b))
 
-int N;
-int book[300001];
-int ans;
+int N, B;
+int mx, ans;
 
 int main(){
     scanf("%d",&N);
     
-    for(int i=0;i<N;i++)
-        scanf("%d",&book[i]);
-    
-    for(int i=N-1;i>=0;i--){
-        if(book[i]==N)N--;
-        else ans++;
+    for(int i=0;i<N;i++){
+        scanf("%d",&B);
+        
+        if(B<mx)ans=max(ans,B);
+        
+        mx=max(mx,B);
     }
     
     printf("%d",ans);
