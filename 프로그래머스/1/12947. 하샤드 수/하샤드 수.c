5607c91cd3636d4b3927e0b128c5c1a86827edb8
Jaeyeong Chwe <zzaekkii@gmail.com>
2026-08-16 22:39:26 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 5.32 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool solution(int x) {
+    int sum = 0;
+    int tmp = x;
+    
+    while (tmp > 0) {
+        sum += tmp % 10;
+        tmp /= 10;
+    }
+    
+    return !(x % sum);
+}
